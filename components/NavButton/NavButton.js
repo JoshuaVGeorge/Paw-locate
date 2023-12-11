ce0db2ef65626a8b433b6ef54ef8b308d3d9e250
Joshua George <joshua.vincent.george@gmail.com>
2023-12-11 01:37:08 -0800

    Added button styling

--- components/NavButton/NavButton.js
+++ components/NavButton/NavButton.js
@@ -3,17 +3,34 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const NavButton = () => {
 	return (
-		<View>
-			<TouchableOpacity>
-				<Text>View Reports</Text>
+		<View style={styles.NavButtonContainer}>
+			<TouchableOpacity style={styles.button1}>
+				<Text style={styles.buttonText}>View Reports</Text>
 			</TouchableOpacity>
-			<TouchableOpacity>
-				<Text>Login</Text>
+			<TouchableOpacity style={styles.button2}>
+				<Text style={styles.buttonText}>Login</Text>
 			</TouchableOpacity>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	NavButtonContainer: {
+		backgroundColor: "#2cdd59",
+		flexDirection: "row",
+		justifyContent: "center",
+	},
+	button1: {
+		backgroundColor: "#DDDDDD",
+		padding: 10,
+	},
+	button2: {
+		backgroundColor: "#DDDDDD",
+		padding: 10,
+	},
+	buttonText: {
+		fontSize: 20,
+	},
+});
 
 export default NavButton;
