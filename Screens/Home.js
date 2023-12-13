579660899dde31f5bd1b658131cdf5c9237b4359
Joshua George <joshua.vincent.george@gmail.com>
2023-12-13 00:13:38 -0800

    Added basic structure to Home

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import NavButton from "../components/NavButton/NavButton";
 
 const Home = () => {
 	return (
-		<View style={StyleSheet.container}>
-			<Text>Home Page</Text>
+		<View style={styles.container}>
+			<View style={styles.content}>
+				<Text style={styles.logo}>Paw Locate</Text>
+				<NavButton />
+			</View>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: { flex: 1 },
+	content: {
+		flex: 1,
+		justifyContent: "center",
+		backgroundColor: "#f2fdf5",
+	},
+	logo: {
+		textAlign: "center",
+		backgroundColor: "#8eece7",
+		paddingBottom: 40,
+		fontSize: 50,
+	},
+});
 
 export default Home;
