df5931e2476a24f119337e00324002ae7d7c2353
Joshua George <joshua.vincent.george@gmail.com>
2023-12-13 00:06:24 -0800

    basic content added

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-const Home = () => {};
+const Home = () => {
+	return (
+		<View style={StyleSheet.container}>
+			<Text>Home Page</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
 
 export default Home;
