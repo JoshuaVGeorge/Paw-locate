5cb21ce6235d2b7ebb885ff8cd0cb8c03545e7d0
Joshua George <joshua.vincent.george@gmail.com>
2023-12-12 22:35:43 -0800

    added safeAreaView at top level

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 import NavButton from "./components/NavButton/NavButton";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.header}>Paw Locate</Text>
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.logo}>Paw Locate</Text>
 			<NavButton style={styles.button} />
-		</View>
+		</SafeAreaView>
 	);
 }
 
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
 		backgroundColor: "#f2fdf5",
 		justifyContent: "center",
 	},
-	header: {
+	logo: {
 		textAlign: "center",
 		backgroundColor: "#8eece7",
 		paddingBottom: 40,
