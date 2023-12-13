5594a53aa7904a5b71f6787ff7ed581c2939c03f
Joshua George <joshua.vincent.george@gmail.com>
2023-12-13 01:19:46 -0800

    boilerplate for login page

---
+++ Screens/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const Login = () => {
+	return (
+		<View>
+			<Text>Login Page</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
