37dc4bcce4fd8cfb84ae6697dfecbc07d3410069
Joshua George <joshua.vincent.george@gmail.com>
2023-12-13 00:00:54 -0800

    converted App to arrow syntax

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
 	SafeAreaView,
 	View,
@@ -8,27 +9,34 @@ import {
 } from "react-native";
 import NavButton from "./components/NavButton/NavButton";
 
-export default function App() {
+const App = () => {
 	return (
 		<SafeAreaView style={styles.container}>
 			<TouchableWithoutFeedback
+				style={styles.container__touchable}
 				onPress={() => {
 					Keyboard.dismiss();
 				}}>
-				<View>
+				<View style={styles.content}>
 					<Text style={styles.logo}>Paw Locate</Text>
-					<NavButton style={styles.button} />
+					<NavButton />
 				</View>
 			</TouchableWithoutFeedback>
 		</SafeAreaView>
 	);
-}
+};
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: "#f2fdf5",
+	},
+	container__touchable: {
+		flex: 1,
+	},
+	content: {
+		flex: 1,
 		justifyContent: "center",
+		backgroundColor: "#f2fdf5",
 	},
 	logo: {
 		textAlign: "center",
@@ -37,3 +45,5 @@ const styles = StyleSheet.create({
 		fontSize: 50,
 	},
 });
+
+export default App;
