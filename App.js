b825f24ed6493128065366df96a9b35176c3771c
Joshua George <joshua.vincent.george@gmail.com>
2023-12-13 22:17:53 -0800

    added stack screen for ViewReports

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./Screens/Home";
 import Login from "./Screens/Login";
+import ViewReports from "./Screens/ViewReports";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const App = () => {
 			<Stack.Navigator initialRouteName="Home">
 				<Stack.Screen name="Home" component={Home} />
 				<Stack.Screen name="Login" component={Login} />
+				<Stack.Screen name="ViewReports" component={ViewReports} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
