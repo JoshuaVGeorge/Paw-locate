4a3f177d1b023db06d5a7e0ce0b1364164eac546
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 14:39:40 -0800

    added dataloading splash screen

--- Screens/ViewReports.js
+++ Screens/ViewReports.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
 import ReportCard from "../components/ReportCard/ReportCard";
 import axios from "axios";
+import * as SplashScreen from "expo-splash-screen";
 
 const ViewReports = ({ navigation }) => {
 	const apiURL = "http://192.168.1.72:8080/reports";
 
+	SplashScreen.preventAutoHideAsync();
+
+	const [appReady, setAppReady] = useState(false);
 	const [reportData, setReportData] = useState();
 
 	useEffect(() => {
@@ -13,22 +17,30 @@ const ViewReports = ({ navigation }) => {
 			.get(apiURL)
 			.then((res) => {
 				setReportData(res.data);
+				setAppReady(true);
 			})
 			.catch((err) => {
 				console.error(err);
 			});
 	}, []);
 
-	if (reportData) {
-		reportData.map((report) => {
-			console.log(report.id);
-		});
+	const checkData = useCallback(async () => {
+		if (appReady) {
+			await SplashScreen.hideAsync();
+		}
+	}, [appReady]);
+
+	if (!appReady) {
+		return null;
 	}
 
 	return (
-		<View style={styles.container}>
+		<View style={styles.container} onLayout={checkData}>
 			<Text>Reports Page</Text>
 			<View style={styles.list}>
+				<ReportCard />
+				<ReportCard />
+				<ReportCard />
 				<ReportCard />
 			</View>
 			<Button
