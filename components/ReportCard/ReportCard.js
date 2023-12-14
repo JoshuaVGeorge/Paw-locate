cc985952ec2ce6d68e8f387a4be9ddae1efdcb88
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 12:32:07 -0800

    boilerplate structure

---
+++ components/ReportCard/ReportCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const ReportCard = () => {
+	const navigation = useNavigation();
+
+	return <View style={styles.navButtonContainer}></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ReportCard;
