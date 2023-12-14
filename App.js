f7134b2e5d83e3705307d3cbdeb069e6ad96e52b
Joshua George <joshua.vincent.george@gmail.com>
2023-12-13 22:09:00 -0800

    created stack screen for Login page

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./Screens/Home";
+import Login from "./Screens/Login";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
+			<Stack.Navigator initialRouteName="Home">
 				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="Login" component={Login} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
