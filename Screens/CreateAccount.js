efb837b34be2da8df12cae1ae85a4367e245edf5
Joshua George <joshua.vincent.george@gmail.com>
2023-12-13 22:27:21 -0800

    boilerplate structure

---
+++ Screens/CreateAccount.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const CreateAccount = () => {
+	return (
+		<View>
+			<Text>Create Account</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateAccount;
