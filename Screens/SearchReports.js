961c28fc6bed7c6a4a4558522d29d0831f6bed16
Joshua George <joshua.vincent.george@gmail.com>
2023-12-13 22:35:06 -0800

    BoilerPlate structure created

---
+++ Screens/SearchReports.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SearchReports = () => {
+	return (
+		<View>
+			<Text>Search Reports</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchReports;
