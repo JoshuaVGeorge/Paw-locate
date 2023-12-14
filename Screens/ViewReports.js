df4513d9f674c261472e16f89a2341d7b654ae53
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 14:24:08 -0800

    loading data from api

--- Screens/ViewReports.js
+++ Screens/ViewReports.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
 import ReportCard from "../components/ReportCard/ReportCard";
+import axios from "axios";
 
 const ViewReports = ({ navigation }) => {
+	const apiURL = "http://192.168.1.72:8080/reports";
+
+	const [reportData, setReportData] = useState();
+
+	useEffect(() => {
+		axios
+			.get(apiURL)
+			.then((res) => {
+				setReportData(res.data);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	if (reportData) {
+		reportData.map((report) => {
+			console.log(report.id);
+		});
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text>Reports Page</Text>
