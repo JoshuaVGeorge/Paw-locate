24505f29116480864d79292fd8e85779e1ae9dea
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 12:33:49 -0800

    imported ReportCard

--- Screens/ViewReports.js
+++ Screens/ViewReports.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import ReportCard from "../components/ReportCard/ReportCard";
 
 const ViewReports = ({ navigation }) => {
 	return (
 		<View>
 			<Text>Reports Page</Text>
+			<ReportCard />
 			<Button
 				title="Search Reports"
 				onPress={() => navigation.navigate("SearchReports")}></Button>
