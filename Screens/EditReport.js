4c3ba23532fa6fcfba382644322de9ecaf6994fe
Joshua George <joshua.vincent.george@gmail.com>
2023-12-15 02:33:55 -0800

    added api dat and passed to form

--- Screens/EditReport.js
+++ Screens/EditReport.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
 	StyleSheet,
 	Text,
@@ -9,16 +9,48 @@ import {
 } from "react-native";
 import ReportForm from "../components/ReportForm/ReportForm";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { API_URL } from "@env";
+import axios from "axios";
 
-const EditReport = ({ navigation }) => {
+const EditReport = ({ navigation, route }) => {
+	SplashScreen.preventAutoHideAsync();
+
+	const { reportId } = route.params;
+
+	const [appReady, setAppReady] = useState(false);
+	const [initialReportData, setInitialReportData] = useState();
+
+	useEffect(() => {
+		axios
+			.get(`${API_URL}/reports/${reportId}`)
+			.then((res) => {
+				setInitialReportData(res.data[0]);
+				setAppReady(true);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	const checkData = useCallback(async () => {
+		if (appReady) {
+			await SplashScreen.hideAsync();
+		}
+	}, [appReady]);
+
+	if (!appReady) {
+		return null;
+	}
+
 	return (
 		<TouchableWithoutFeedback
 			onPress={() => {
 				Keyboard.dismiss();
 			}}>
 			<SafeAreaView style={styles.container}>
-				<ReportForm />
-				<View style={styles.button__container}>
+				<ReportForm isEdit={true} reportData={initialReportData} />
+				<View style={styles.button__container} onLayout={checkData}>
 					<TouchableOpacity
 						style={styles.button}
 						onPress={() => {
