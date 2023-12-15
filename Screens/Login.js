792a56b738a47f3a97c19aaa97fbc13af02426b8
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 18:29:49 -0800

    imported LoginForm

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import LoginForm from "../components/LoginForm/LoginForm";
 
 const Login = ({ navigation }) => {
 	return (
 		<View>
 			<Text>Login Page</Text>
+			<LoginForm />
 			<Button
 				title="create account"
 				onPress={() => navigation.navigate("CreateAccount")}></Button>
