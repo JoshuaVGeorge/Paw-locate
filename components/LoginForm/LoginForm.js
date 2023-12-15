e2721e27517faad313a34354544cb60f157f7833
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 18:29:39 -0800

    boilerplate structure created

---
+++ components/LoginForm/LoginForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const LoginForm = () => {
+	return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default LoginForm;
