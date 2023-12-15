96a9b860a1f4fff0191229f05e796cd59948550a
Joshua George <joshua.vincent.george@gmail.com>
2023-12-15 01:26:11 -0800

    implemented goTo and dynamic data

--- components/ListTile/ListTile.js
+++ components/ListTile/ListTile.js
@@ -2,15 +2,23 @@ import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const ListTile = ({ primaryLabel, sndLabel }) => {
+const ListTile = ({ primaryLabel, sndLabel, goTo, isTip, reportId }) => {
 	const navigation = useNavigation();
 
 	return (
 		<View style={styles.container}>
-			<TouchableOpacity style={[styles.primary__tile, styles.tile]}>
+			<TouchableOpacity
+				style={[styles.primary__tile, styles.tile]}
+				onPress={() => {
+					goTo(primaryLabel, reportId, isTip);
+				}}>
 				<Text style={styles.container}>{primaryLabel}</Text>
 			</TouchableOpacity>
-			<TouchableOpacity style={[styles.snd__tile, styles.tile]}>
+			<TouchableOpacity
+				style={[styles.snd__tile, styles.tile]}
+				onPress={() => {
+					goTo(sndLabel, reportId);
+				}}>
 				<Text style={styles.container}>{sndLabel}</Text>
 			</TouchableOpacity>
 		</View>
