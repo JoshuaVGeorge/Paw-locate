a1878177a0c5365226f36efd396202679615cbf3
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 18:11:52 -0800

    styling added

--- components/TipCard/TipCard.js
+++ components/TipCard/TipCard.js
@@ -2,25 +2,37 @@ import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 
 const TipCard = ({ id, img, status, text_data, user_name }) => {
-	console.log(user_name);
 	return (
 		<View style={styles.container}>
 			<View style={styles.title}>
 				<Text>{user_name}</Text>
 			</View>
 			<View style={styles.content}>
-				<Text style={styles.text}> {text_data}</Text>
 				<Image
 					style={styles.image}
 					source={{
 						uri: img,
 					}}
 				/>
+				<Text style={styles.text}>{text_data}</Text>
 			</View>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	title: {
+		paddingBottom: 10,
+	},
+	content: {
+		flex: 1,
+		height: 300,
+	},
+	image: {
+		width: "50%",
+		height: "50%",
+		marginBottom: 5,
+	},
+});
 
 export default TipCard;
