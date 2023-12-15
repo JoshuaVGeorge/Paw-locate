317d52e22b470fb38e96b1055aa75d10be5de49c
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 21:03:38 -0800

    jsx stucture added

--- Screens/CreateAccount.js
+++ Screens/CreateAccount.js
@@ -1,15 +1,38 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import {
+	View,
+	StyleSheet,
+	Text,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from "react-native";
+import CreateAccountForm from "../components/CreateAccountForm/CreateAccountForm";
 
-const CreateAccount = ({ navigation }) => {
+const Login = ({ navigation }) => {
 	return (
-		<View>
-			<Text>Create Account</Text>
-			<Button title="login" onPress={() => navigation.goBack()}></Button>
-		</View>
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}>
+			<View style={styles.container}>
+				<Text style={styles.title}>PAW LOCATE</Text>
+				<CreateAccountForm />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 40,
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 50,
+		paddingBottom: 50,
+		textAlign: "center",
+	},
+});
 
-export default CreateAccount;
+export default Login;
