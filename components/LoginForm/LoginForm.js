ef91a972dcd36e10ef4a878b9988b60417563518
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 20:22:06 -0800

    added login button

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	TouchableOpacity,
+} from "react-native";
 
 const LoginForm = () => {
 	return (
 		<View style={styles.container}>
 			<TextInput style={styles.input} placeholder="Username" />
 			<TextInput style={styles.input} placeholder="Password" />
+			<TouchableOpacity style={styles.button}>
+				<Text>Login</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
@@ -20,6 +29,12 @@ const styles = StyleSheet.create({
 		borderWidth: 2,
 		padding: 10,
 		marginBottom: 30,
+	},
+	button: {
+		alignSelf: "flex-end",
+		paddingVertical: 5,
+		paddingHorizontal: 20,
+		borderWidth: 2,
 	},
 });
 
