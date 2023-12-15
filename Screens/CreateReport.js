d26a04beabe764b498ca0fe9e6cd4d70d8eeadd5
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 23:59:16 -0800

    imported ReportForm & added keyboard dismiss

--- Screens/CreateReport.js
+++ Screens/CreateReport.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import {
+	View,
+	StyleSheet,
+	Text,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from "react-native";
+import ReportForm from "../components/ReportForm/ReportForm";
 
 const CreateReport = () => {
 	return (
-		<View>
-			<Text>Create Report</Text>
-		</View>
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}>
+			<View style={styles.container}>
+				<Text>Create Report</Text>
+				<ReportForm />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 10,
+	},
+});
 
 export default CreateReport;
