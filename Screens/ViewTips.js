5579e77cbc814d1ae1f378fa77dd882cb7c428c0
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 17:23:24 -0800

    added api data

--- Screens/ViewTips.js
+++ Screens/ViewTips.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import axios from "axios";
 
-const ViewTips = ({ navigation }) => {
+const ViewTips = ({ navigation, route }) => {
+	const apiURL = "http://192.168.1.72:8080/reports";
+	const { reportId } = route.params;
+
+	SplashScreen.preventAutoHideAsync();
+
+	const [appReady, setAppReady] = useState(false);
+	const [tipData, setTipData] = useState();
+
+	useEffect(() => {
+		axios
+			.get(`${apiURL}/${reportId}/tips`)
+			.then((res) => {
+				// setReportData(res.data[0]);
+				// setAppReady(true);
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	const checkData = useCallback(async () => {
+		if (appReady) {
+			await SplashScreen.hideAsync();
+		}
+	}, [appReady]);
+
+	if (!appReady) {
+		return null;
+	}
+
 	return (
 		<View>
 			<Text>View Tips</Text>
