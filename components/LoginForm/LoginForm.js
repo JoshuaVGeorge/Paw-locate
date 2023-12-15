9716bdc6c4dcf299a85372297e46a7df2591dd25
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 20:11:37 -0800

    added basic structure with placeholder

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	TouchableWithoutFeedback,
+} from "react-native";
 
 const LoginForm = () => {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<TextInput style={styles.input} placeholder="Username" />
+			<TextInput style={styles.input} placeholder="Password" />
+		</View>
+	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		justifyContent: "center",
+		marginBottom: 30,
+	},
+	input: {
+		height: 40,
+		borderWidth: 2,
+		padding: 10,
+		marginBottom: 30,
+	},
+});
 
 export default LoginForm;
