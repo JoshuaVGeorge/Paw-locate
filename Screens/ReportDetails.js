a9bafb836f5983b6de6e82961c4d31417e5a83d9
Joshua George <joshua.vincent.george@gmail.com>
2023-12-14 16:43:04 -0800

    impoted reportDataCArd

--- Screens/ReportDetails.js
+++ Screens/ReportDetails.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import axios from "axios";
+import * as SplashScreen from "expo-splash-screen";
+import ReportDetailsCard from "../components/ReportDetailsCard/ReportDetailsCard";
 
 const ReportDetails = ({ navigation, route }) => {
+	const apiURL = "http://192.168.1.72:8080/reports";
+	const { reportId, userId } = route.params;
+
+	SplashScreen.preventAutoHideAsync();
+
+	const [appReady, setAppReady] = useState(false);
+	const [reportData, setReportData] = useState();
+
+	useEffect(() => {
+		axios
+			.get(`${apiURL}/${reportId}`)
+			.then((res) => {
+				setReportData(res.data[0]);
+				setAppReady(true);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	const checkData = useCallback(async () => {
+		if (appReady) {
+			await SplashScreen.hideAsync();
+		}
+	}, [appReady]);
+
+	if (!appReady) {
+		return null;
+	}
+
 	return (
-		<View>
-			<Text>Report Details</Text>
+		<View style={styles.container} onLayout={checkData}>
+			<ReportDetailsCard
+				petName={reportData.pet_name}
+				petImg={reportData.pet_image}
+				description={reportData.description}
+				location={reportData.location_data}
+				status={reportData.status}
+			/>
 			<Button
 				title="Search Reports"
 				onPress={() => {
@@ -27,6 +66,10 @@ const ReportDetails = ({ navigation, route }) => {
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		paddingHorizontal: 10,
+	},
+});
 
 export default ReportDetails;
