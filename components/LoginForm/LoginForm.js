d20c1344c0386fecf7ed3e046ccef298f5c564b3
Joshua George <joshua.vincent.george@gmail.com>
2023-12-16 08:32:20 -0800

    submit form post request func added

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	StyleSheet,
 	Text,
 	View,
 	TextInput,
 	TouchableOpacity,
+	Alert,
 } from "react-native";
+import { API_URL } from "@env";
+import axios from "axios";
 
 const LoginForm = () => {
+	const [usernameValue, setUsernameValue] = useState("");
+	const [passwordValue, setPasswordValue] = useState("");
+
+	const submitForm = () => {
+		const formData = { user_name: usernameValue, password: passwordValue };
+		axios
+			.post(`${API_URL}/profile`, formData)
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	return (
 		<View style={styles.container}>
-			<TextInput style={styles.input} placeholder="Username" maxLength={40} />
+			<TextInput
+				style={styles.input}
+				placeholder="Username"
+				maxLength={40}
+				value={usernameValue}
+				onChangeText={(text) => setUsernameValue(text)}
+			/>
 			<TextInput
 				style={styles.input}
 				placeholder="Password"
 				maxLength={20}
 				secureTextEntry
+				value={passwordValue}
+				onChangeText={(text) => setPasswordValue(text)}
 			/>
-			<TouchableOpacity style={styles.button}>
+			<TouchableOpacity style={styles.button} onPress={submitForm}>
 				<Text>Login</Text>
 			</TouchableOpacity>
 		</View>
