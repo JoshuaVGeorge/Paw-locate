f5091a1f10a6c6fbdbf22f7df1ccb0259e9c4845
Joshua George <joshua.vincent.george@gmail.com>
2023-12-17 13:10:58 -0800

    imported TipForm and route params

--- Screens/AddTip.js
+++ Screens/AddTip.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import TipForm from "../components/TipForm/TipForm";
 
-const AddTip = ({ navigation }) => {
+const AddTip = ({ navigation, route }) => {
+	const { reportId } = route.params;
 	return (
-		<View>
-			<Text>add tip page</Text>
-			<Button
-				title="go back"
-				onPress={() => {
-					navigation.goBack();
-				}}
-			/>
+		<View style={styles.container}>
+			<TipForm reportId={reportId} />
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 10,
+	},
+});
 
 export default AddTip;
