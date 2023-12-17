c9f0cffefd58edba0a0f0395e4c599fb2dc76315
Joshua George <joshua.vincent.george@gmail.com>
2023-12-16 18:03:54 -0800

    stored token and user info in storage

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
 	StyleSheet,
 	Text,
@@ -10,25 +10,66 @@ import {
 import { API_URL } from "@env";
 import axios from "axios";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LoginForm = () => {
 	const navigation = useNavigation();
 	const [usernameValue, setUsernameValue] = useState("");
 	const [passwordValue, setPasswordValue] = useState("");
 
+	const [userToken, setUserToken] = useState("");
+	const [userInfo, setUserInfo] = useState("");
+
+	useEffect(() => {
+		checkLoggedIn();
+		if (userToken && userInfo) {
+			navigation.navigate("Profile", {
+				userInfo: userInfo,
+				userToken: userToken,
+			});
+		}
+	});
+
 	const loginUser = () => {
 		const formData = { user_name: usernameValue, password: passwordValue };
 		axios
 			.post(`${API_URL}/profile`, formData)
 			.then((res) => {
-				navigation.navigate("Profile", {
-					userToken: res.data.token,
-					profileData: res.data.profile,
+				let data = res.data;
+				setUserToken(data.token);
+				setUserInfo(data.profile[0].id);
+				return data;
+			})
+			.then((res) => {
+				storeData(res);
+				navigation.replace("Profile", {
+					userInfo: userInfo,
+					userToken: userToken,
 				});
 			})
 			.catch((err) => {
 				Alert.alert("Username or Password does not match");
 			});
+	};
+
+	const storeData = async (data) => {
+		try {
+			await AsyncStorage.setItem("userToken", data.token);
+			await AsyncStorage.setItem("userInfo", `${data.profile[0].id}`);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	const checkLoggedIn = async () => {
+		try {
+			let asyncUserToken = await AsyncStorage.getItem("userToken");
+			let asyncUserinfo = await AsyncStorage.getItem("userInfo");
+			setUserToken(asyncUserToken);
+			setUserInfo(asyncUserinfo);
+		} catch (e) {
+			console.log(e);
+		}
 	};
 
 	return (
