7e22442d7893fce0c6209ab6ecbe080fd481ab01
Joshua George <joshua.vincent.george@gmail.com>
2024-01-25 16:59:13 -0800

    added keyboard.dismiss

--- Screens/AddTip.js
+++ Screens/AddTip.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { View, StyleSheet, Alert } from "react-native";
+import {
+	View,
+	StyleSheet,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from "react-native";
 import TipForm from "../components/TipForm/TipForm";
 
 const AddTip = ({ route }) => {
 	const { reportId, userId } = route.params;
 
 	return (
-		<View style={styles.container}>
-			<TipForm reportId={reportId} userId={userId} />
-		</View>
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}>
+			<View style={styles.container}>
+				<TipForm reportId={reportId} userId={userId} />
+			</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
